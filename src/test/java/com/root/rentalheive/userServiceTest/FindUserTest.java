81da1bcdf6647287ab30fa95ff068cb559882420
Abdelaziz100Dev <ez.zahed.abdelaziz@gmail.com>
2023-11-19 17:14:43 +0100

    adding conception 2

--- src/test/java/com/root/rentalheive/userServiceTest/FindUserTest.java
+++ src/test/java/com/root/rentalheive/userServiceTest/FindUserTest.java
@@ -1,5 +1,6 @@
 package com.root.rentalheive;
 
+import com.root.rentalheive.repositories.UserRepository;
 import com.root.rentalheive.services.UserService;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -7,10 +8,12 @@ import static org.mockito.Mockito.mock;
 
 public class UserServiceTest {
 
+    static UserService userService;
+    static UserRepository userRepository;
     @BeforeAll
     public static void init() throws Exception{
         userService = new UserService();
-        userRepository = mock(UserRepositoryImpl.class);
+        userRepository = mock(UserRepository.class);
 
     }
 }
