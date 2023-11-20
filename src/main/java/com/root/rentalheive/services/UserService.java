37eecf1b030eb47850c391a0a6e8ecc529ccd1ee
Abdelaziz100Dev <ez.zahed.abdelaziz@gmail.com>
2023-11-20 16:43:23 +0100

    adding user testing

--- src/main/java/com/root/rentalheive/services/UserService.java
+++ src/main/java/com/root/rentalheive/services/UserService.java
@@ -1,2 +1,23 @@
-package com.root.rentalheive.services;public class UserService {
+package com.root.rentalheive.services;
+
+import com.root.rentalheive.entities.User;
+import com.root.rentalheive.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public Optional<User> findUserById(Long id) {
+       return userRepository.findById(id);
+    }
+
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
 }
