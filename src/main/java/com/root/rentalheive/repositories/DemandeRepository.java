16d9a9abcbfe299bbf6cb2db08c79968f39fa571
Abdelaziz100Dev <ez.zahed.abdelaziz@gmail.com>
2023-11-25 17:24:40 +0100

    adding decliningDemand & demandServiceTesting

--- src/main/java/com/root/rentalheive/repositories/DemandeRepository.java
+++ src/main/java/com/root/rentalheive/repositories/DemandeRepository.java
@@ -1,9 +1,13 @@
 package com.root.rentalheive.repositories;
 
 import com.root.rentalheive.entities.Demand;
+import com.root.rentalheive.enums.DemandStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -11,4 +15,8 @@ import java.util.List;
 @Repository
 public interface DemandeRepository extends JpaRepository<Demand, Long> {
     public Demand getDemandById(Long id);
+    @Transactional
+    @Modifying
+    @Query("UPDATE Demand d SET d.status = :status WHERE d.id = :id")
+     int declineDemand(Long id,@Param("status") DemandStatus status);
 }
