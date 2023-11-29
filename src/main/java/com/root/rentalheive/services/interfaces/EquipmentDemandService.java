3abf3bf6076429f8544faa7fbe6af2aa68833094
ayoub ouabi <ayoub.ouabi06@gmail.com>
2023-11-29 15:02:48 +0100

    working on intefaces

--- src/main/java/com/root/rentalheive/services/interfaces/EquipmentDemandService.java
+++ src/main/java/com/root/rentalheive/services/interfaces/EquipmentDemandService.java
@@ -7,9 +7,12 @@ import java.text.ParseException;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface EquipmentDemandService {
     EquipmentDemand save(EquipmentDemand equipmentDemand);
-    List<EquipmentDemand> checkAvailability(LocalDate startDate, LocalDate endDate , Equipment equipment) throws ParseException;
+    List<EquipmentDemand> getAllEquipmentDemand();
+    Optional<Long> checkAvailability(LocalDate startDate, LocalDate endDate , Long equipment) throws ParseException;
+   // List<EquipmentDemand> checkAvailability(LocalDate startDate, LocalDate endDate , Equipment equipment) throws ParseException;
 }
