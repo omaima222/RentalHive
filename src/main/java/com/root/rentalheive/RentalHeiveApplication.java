4a11e07d7c498df583bcc110059b9ae232443ad6
ayoub ouabi <ayoub.ouabi06@gmail.com>
2023-12-05 09:48:55 +0100

    add cosFilter Bean

--- src/main/java/com/root/rentalheive/RentalHeiveApplication.java
+++ src/main/java/com/root/rentalheive/RentalHeiveApplication.java
@@ -6,17 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class RentalHeiveApplication {
 
-   /* @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }*/
+
     public static void main(String[] args) {
 //        System.setProperty("liquibase.secureParsing", "false");
         SpringApplication.run(RentalHeiveApplication.class, args);
     }
-
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
